cb85d0f9fd17316bbadee6d15955101683bf715c
theofal <46688217+theofal@users.noreply.github.com>
2022-03-27 21:58:47 +0200

    GHA tests

--- main_test.go
+++ main_test.go
@@ -3,15 +3,25 @@ package main
 import (
 	_ "go.uber.org/zap"
 	_ "go.uber.org/zap/zapcore"
+	"log"
+	"os"
 	"strconv"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestGetDotEnvVar(t *testing.T) {
+func TestMain(m *testing.M) {
 	Logger = InitLogger()
 	Logger.Sync()
+	log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	log.Println("Do stuff AFTER the tests!")
+
+	os.Exit(exitVal)
+}
+
+func TestGetDotEnvVar(t *testing.T) {
 	result := getDotEnvVar("TEST_ARG")
 	if result != "BrUh" {
 		t.Errorf("FAIL: Expected %v, got %v", "BrUh", result)
@@ -19,8 +29,6 @@ func TestGetDotEnvVar(t *testing.T) {
 }
 
 func TestParseDate(t *testing.T) {
-	Logger = InitLogger()
-	Logger.Sync()
 	expected := strconv.Itoa(time.Now().Add(-time.Hour * (time.Duration(2) * 24)).Day())
 	result := ParseDate("Il y a 2 jours")
 	if !strings.Contains(result, expected) {
