f5913089c9c0cb41d3851d8c29b5886338f11afd
theofal <46688217+theofal@users.noreply.github.com>
2022-03-27 14:48:00 +0200

    GH tests

--- main_test.go
+++ main_test.go
@@ -8,8 +8,6 @@ import (
 )
 
 func TestGetDotEnvVar(t *testing.T) {
-	InitLogger()
-
 	result := getDotEnvVar("TEST_ARG")
 	if result != "BrUh" {
 		t.Errorf("FAIL: Expected %v, got %v", "BrUh", result)
@@ -17,8 +15,6 @@ func TestGetDotEnvVar(t *testing.T) {
 }
 
 func TestParseDate(t *testing.T) {
-	InitLogger()
-
 	expected := strconv.Itoa(time.Now().Add(-time.Hour * (time.Duration(2) * 24)).Day())
 	result := ParseDate("Il y a 2 jours")
 	if !strings.Contains(result, expected) {
